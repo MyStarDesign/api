1c9ec7a0d8b7bfab8296c20d32b9a67f56ff8c8e
Jon Greer <jongreer@Sams-MacBook-Pro.local>
2018-07-23 12:39:23 +0100

    add middleware functions

--- app.js
+++ app.js
@@ -2,6 +2,35 @@
 
 var express = require('express');
 var app = express();
+var jsonParser = require("body-parser").json;
+
+var jsonCheck = function(req, res, next) {
+  if(req.body) {
+    console.log("The sky is", req.body.colour);
+  } else {
+    console.log("Thre is no body property");
+  }
+  next();
+}
+
+app.use(jsonCheck);
+app.use(jsonParser());
+app.use(jsonCheck);
+
+//app.use(function(req, res, next){
+//  console.log('Log', req.query.colour);
+//  next();
+//});
+
+//app.use(function(req, res, next){
+//  req.myMessage = "Hello, middleware 2";
+//  next();
+//});
+//
+//app.use(function(req, res, next){
+//  console.log(req.myMessage);
+//  next();
+//});
 
 var port = process.env.PORT || 3000;
 
